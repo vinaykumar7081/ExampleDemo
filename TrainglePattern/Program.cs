ed9363dd63f49534e0728a0d85ffae3054e93eed
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-04 11:50:32 +0530

    [VINAY] Add:program Successfully Execute

---
+++ TrainglePattern/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+class TrainglePattern
+{
+    public static void Main(String[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            int A = 65;
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write((char)A++);
+            }
+            Console.WriteLine(" ");
+        }
+    
+    
+    }
+}
