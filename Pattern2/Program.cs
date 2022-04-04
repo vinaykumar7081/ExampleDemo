f6803e6977ea5163dee41fdd5e71a219ae8d6aae
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-04 17:28:39 +0530

    [VINAY] Add: New File Udated

--- Pattern2/Program.cs
+++ Pattern2/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using System;
+class Pattern2
+{
+    public static void Main(String[] args)
+    {
+        int a = 17;
+        int count = 0;
+        for (int i = 1; i <= a; i++)
+        {
+            if (a % i == 0)
+            {
+                count++;
+            }
+        }
+            if (count == 2)
+            {
+                Console.WriteLine("Given Number is Prime number");
+            }
+
+            else
+            {
+                Console.WriteLine("Given number is not prime");
+            }
+    }
+
+        
+
+    }
+
