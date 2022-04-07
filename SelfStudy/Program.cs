e3824c7970f24bd8042cad13f8ec6b9807667948
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-07 18:37:06 +0530

    [VINAY] Add: concate first Name and Last Name

---
+++ SelfStudy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SelfStudy
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Entre Name");
+            string firstName=Console.ReadLine();
+            string lastName=Console.ReadLine();
+            string fullName=firstName+" "+lastName;
+            Console.WriteLine(fullName);
+        }
+    
+    }
+}
